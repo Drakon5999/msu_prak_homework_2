2596c7227e812a2b79f4c4cd69f525d2ceb622f4
Ivan Lozinskii <ar7is7@yandex-team.ru>
2019-09-28 14:02:19 +0300

    fourth task

--- graph_impl.py
+++ graph_impl.py
@@ -40,6 +40,8 @@ class Graph:
     def dfs(self, start_vertex=0):
         """
         visit graphs vertexes with dfs algorithm
+        non recursive version of dfs algorithm described here
+        https://ru.wikipedia.org/wiki/%D0%9F%D0%BE%D0%B8%D1%81%D0%BA_%D0%B2_%D0%B3%D0%BB%D1%83%D0%B1%D0%B8%D0%BD%D1%83
         :param start_vertex
         :return: void
         """
